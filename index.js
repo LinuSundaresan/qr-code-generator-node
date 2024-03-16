f0333ac17e29aa37f6c7acefb59d13ddc0225e5f
linu <linusundaresan@gmail.com>
2024-03-16 16:26:35 +0530

    packeges installed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("here");
+/* use inquirer npm package
+use qr-image npm package
+create text file to save user input using native fs module */
+
+import inquirer from 'inquirer';
+//var qr = require('qr-image');
+
+inquirer
+  .prompt([
+    {
+        message: "Type in your URL",
+        name: "URL",
+    },
+    ])
+  .then((answers) => {
+    console.log(answers);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
